b645b27a3f8ce0e2493a9e93a065f1e44f8a9011
Glen Willis <glenwillis@gmail.com>
2017-08-29 21:18:33 +0800

    Created a way to get pi to a certain amount of digits.

--- KaranNumberProjects/PiToNthDigit.cs
+++ KaranNumberProjects/PiToNthDigit.cs
@@ -9,12 +9,13 @@ namespace KaranNumberProjects
     class PiToNthDigit
     {
         int count = 0;
+        const int ARRINT = 2000;
+        const int SCALE = 10000;
+
         public void CalculatePiToNthDigit()
         {
             int digit = 0;
             bool correctInput = false;
-            double piCalculated;
-            
 
             while (!correctInput)
             {
@@ -34,17 +35,43 @@ namespace KaranNumberProjects
                 {
                     Console.WriteLine("Enter an integer");
                 }
-            }
-
+                
+        }
+        
             Console.WriteLine("Finding pi to {0}th digit", digit);
 
             Console.WriteLine(piReturn(digit));
             Console.ReadKey();
         }
 
-        double piReturn(int digit)
+        string piReturn(int digits)
         {
-            return 1 + digit / (2.0 * digit + 1) * piReturn(digit + 1);
+            StringBuilder pi = new StringBuilder();
+            int[] array = new int[digits + 1];
+            int carry = 0;
+
+            for (int i = 0; i < digits; ++i)
+            {
+                array[i] = ARRINT;
+            }
+
+            for (int i = digits; i > 0; i-= 14)
+            {
+                int sum = 0;
+                for (int j = i; j > 0; --j)
+                {
+                    sum = sum * j + SCALE * array[j];
+                    array[j] = sum % (j * 2 - 1);
+                    sum /= j * 2 - 1;
+                }
+                pi.Append((carry + sum / SCALE).ToString());
+                carry = sum % SCALE;
+            }
+            string piString = pi.ToString().Remove(1, 2);
+
+            return "3." + piString;
         }
+
+
     }
 }
--- KaranNumberProjects/Program.cs
+++ KaranNumberProjects/Program.cs
@@ -12,6 +12,24 @@ namespace KaranNumberProjects
         {
             PiToNthDigit piToNthDigit = new PiToNthDigit();
             piToNthDigit.CalculatePiToNthDigit();
+
+            /*int count = 0;
+            int total = Recursive(5, ref count);
+
+            Console.WriteLine(total);
+            Console.WriteLine(count);
+            Console.ReadKey();
+            */
+        }
+
+        static int Recursive(int value, ref int count)
+        {
+            count++;
+            if(value >= 10)
+            {
+                return value;
+            }
+            return Recursive(value + 1, ref count);
         }
     }
 }
