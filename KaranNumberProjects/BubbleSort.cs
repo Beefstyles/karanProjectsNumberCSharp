21ea9b1c58d4f3ee52c4283bd5a75e68b6ceea94
Glen Willis <glenwillis@gmail.com>
2017-09-20 21:40:04 +0800

    Implemented bubble sort.

--- KaranNumberProjects/BubbleSort.cs
+++ KaranNumberProjects/BubbleSort.cs
@@ -13,31 +13,40 @@ namespace KaranNumberProjects
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            int[] arrayToBeSorted = { 14, 33, 27, 35, 10 };
+            int[] arrayToBeSorted = { 14, 33, 27, 35, 10, 12 ,256 ,45765, 56454,3253543,576,876,2, 53, 543, 1,2,3,4, 43435,867,769,456987,7845,654,56,435876,75675,789,567,756,876,78,978,56756,765,897,32,324,4354,65,796,-2 };
             string arrayToBeSortedStr = "";
             string sortedArray = "";
+            int counter = 0;
 
             foreach (var item in arrayToBeSorted)
             {
                 arrayToBeSortedStr += " " + item.ToString();
             }
-            bool arrayIsSorted = false;
+            bool arraySwapped = true;
 
             Console.WriteLine("Sorting array {0}", arrayToBeSortedStr);
 
-            while (!arrayIsSorted)
+            for (int j = 0; j < arrayToBeSorted.Length - 1; j++)
             {
-                for (int i = 0; i < arrayToBeSorted.Length - 1; i++)
+                counter++;
+                arraySwapped = true;
+                while (arraySwapped)
                 {
-                    Console.WriteLine("Comparing {0} and {1}", arrayToBeSorted[i], arrayToBeSorted[i + 1]);
-                    if (arrayToBeSorted[i] > arrayToBeSorted[i + 1])
+                    for (int i = 0; i < arrayToBeSorted.Length - 1; i++)
                     {
-                        arrayToBeSorted[i] = arrayToBeSorted[i + 1];
-                        arrayIsSorted = false;
-                    }
-                    else
-                    {
-                        arrayIsSorted = true;
+                        if (arrayToBeSorted[i] > arrayToBeSorted[i + 1])
+                        {
+                            int temp = arrayToBeSorted[i];
+                            arrayToBeSorted[i] = arrayToBeSorted[i + 1];
+                            arrayToBeSorted[i + 1] = temp;
+                            arraySwapped = true;
+                            Console.WriteLine("Switching {0} and {1}", arrayToBeSorted[i], arrayToBeSorted[i + 1]);
+                        }
+                        else
+                        {
+                            arraySwapped = false;
+                            Console.WriteLine("Not switching {0} and {1}", arrayToBeSorted[i], arrayToBeSorted[i + 1]);
+                        }
                     }
                 }
             }
@@ -47,9 +56,10 @@ namespace KaranNumberProjects
                 sortedArray += " " + item.ToString();
             }
 
-            Console.WriteLine("Sorted array {0}", sortedArray);
-            Console.WriteLine("Sorted array in: {0}", stopwatch.Elapsed);
+            Console.WriteLine("Sorted array is now: {0}", sortedArray);
+            Console.WriteLine("Sorted array in: {0} milliseconds and {1} iterations", stopwatch.Elapsed.Milliseconds, counter);
             Console.ReadKey();
         }
     }
+
 }
