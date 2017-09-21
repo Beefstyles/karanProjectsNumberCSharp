ba0ecc0d6259664f737a090dd56e3816b1f68d96
Glen Willis <glenwillis@gmail.com>
2017-09-21 21:17:45 +0800

    Start of the merge sort implementation.

---
+++ KaranNumberProjects/MergeSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaranNumberProjects
+{
+    class MergeSort
+    {
+        public static void MergeSortImplementation()
+        {
+            int[] arrayToBeSorted = { 14, 33, 27, 10, 35, 19, 42, 44};
+
+            for (int i = arrayToBeSorted.Length; i <= 1; i/= 2)
+            {
+
+            }
+        }
+    }
+}
