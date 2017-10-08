c1b12d7a237a15ee46da4b5beb3c383d08543671
Glen Willis <glenwillis@gmail.com>
2017-10-08 18:41:41 +0800

    Started the pig latin interpretation.

---
+++ KaranNumberProjects/PigLatin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaranNumberProjects
+{
+    class PigLatin
+    {
+        public static void PigLatinImplementation()
+        {
+            string inputString = "";
+            string reversedString = "";
+
+            Console.WriteLine("Enter an string");
+            inputString = Console.ReadLine();
+            reversedString = ReturnPigLatin(inputString);
+
+            Console.WriteLine("The input string is {0} and pig latin version is {1}", inputString, reversedString);
+            Console.ReadKey();
+        }
+
+        private static string ReturnPigLatin(string inputString)
+        {
+            char[] inputCharArray = inputString.ToArray();
+        }
+    }
+}
