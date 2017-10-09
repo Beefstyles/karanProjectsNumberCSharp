638aec90f20daa68416934371f4caee61246033f
Glen Willis <glenwillis@gmail.com>
2017-10-09 20:48:11 +0800

    Can now handle three letter consonants.

--- KaranNumberProjects/PigLatin.cs
+++ KaranNumberProjects/PigLatin.cs
@@ -10,7 +10,7 @@ namespace KaranNumberProjects
     {
         static char[] listOfVowels = "aeiou".ToArray();
         static char[] listOfConstanants = "bcdfghjklmnpqrstvwxyz".ToArray();
-        static string[] listOfConstonantClusters = { "ch", "sh", "sm", "th", "st", "scr" };
+        static string[] listOfConstonantClusters = { "ch", "sh", "sm", "th", "st", "sc", "scr" };
         public static void PigLatinImplementation()
         {
             string inputString = "";
@@ -28,15 +28,26 @@ namespace KaranNumberProjects
         {
             char[] inputCharArray = inputString.ToArray();
 
-            Console.WriteLine(inputCharArray[0].ToString() + inputCharArray[1].ToString());
-            int posCluster = Array.IndexOf(listOfConstonantClusters, (inputCharArray[0].ToString() + inputCharArray[1].ToString()));
+
+            int posCluster = Array.IndexOf(listOfConstonantClusters, (inputCharArray[0].ToString() + inputCharArray[1].ToString()+ inputCharArray[2].ToString()));
+
+            //If >1 then first three letters make up consonant cluster
+            if (posCluster > -1)
+            {
+                inputString = inputString.Remove(0, 3);
+                inputString += (inputCharArray[0].ToString() + inputCharArray[1].ToString() + inputCharArray[2].ToString() + "ay");
+                Console.WriteLine("Using three letter consonant cluster rule");
+                return inputString;
+            }
+
+            posCluster = Array.IndexOf(listOfConstonantClusters, (inputCharArray[0].ToString() + inputCharArray[1].ToString()));
 
             //If >1 then first two letters make up consonant cluster
             if (posCluster > -1)
             {
                 inputString = inputString.Remove(0, 2);
                 inputString += (inputCharArray[0].ToString() + inputCharArray[1].ToString() + "ay");
-                Console.WriteLine("Using consonant cluster rule");
+                Console.WriteLine("Using two letter consonant cluster rule");
                 return inputString;
             }
             //If >1 then first letter is a vowel
