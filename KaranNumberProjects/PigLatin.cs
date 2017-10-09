2993d7b5738f1da2fc12c6c1ff6848b255b398f4
Glen Willis <glenwillis@gmail.com>
2017-10-09 20:43:37 +0800

    Added consonant cluster handling.

--- KaranNumberProjects/PigLatin.cs
+++ KaranNumberProjects/PigLatin.cs
@@ -10,6 +10,7 @@ namespace KaranNumberProjects
     {
         static char[] listOfVowels = "aeiou".ToArray();
         static char[] listOfConstanants = "bcdfghjklmnpqrstvwxyz".ToArray();
+        static string[] listOfConstonantClusters = { "ch", "sh", "sm", "th", "st", "scr" };
         public static void PigLatinImplementation()
         {
             string inputString = "";
@@ -26,19 +27,36 @@ namespace KaranNumberProjects
         private static string ReturnPigLatin(string inputString)
         {
             char[] inputCharArray = inputString.ToArray();
+
+            Console.WriteLine(inputCharArray[0].ToString() + inputCharArray[1].ToString());
+            int posCluster = Array.IndexOf(listOfConstonantClusters, (inputCharArray[0].ToString() + inputCharArray[1].ToString()));
+
+            //If >1 then first two letters make up consonant cluster
+            if (posCluster > -1)
+            {
+                inputString = inputString.Remove(0, 2);
+                inputString += (inputCharArray[0].ToString() + inputCharArray[1].ToString() + "ay");
+                Console.WriteLine("Using consonant cluster rule");
+                return inputString;
+            }
             //If >1 then first letter is a vowel
-            int pos = Array.IndexOf(listOfVowels, inputCharArray[0]);
-            if(pos > -1)
+            int posVowel = Array.IndexOf(listOfVowels, inputCharArray[0]);
+
+            if(posVowel > -1)
             {
                 inputString += "way";
+                Console.WriteLine("Using vowel rule");
+                return inputString;
             }
 
-            //If >1 then first letter is a vowel
-            pos = Array.IndexOf(listOfConstanants, inputCharArray[0]);
-            if (pos > -1)
+            //If >1 then first letter is a consonant
+            int posConst = Array.IndexOf(listOfConstanants, inputCharArray[0]);
+            if (posConst > -1)
             {
                 inputString = inputString.Remove(0, 1);
                 inputString += (inputCharArray[0] + "ay");
+                Console.WriteLine("Using consonant rule");
+                return inputString;
             }
 
             return inputString;
