f7b3cee2ea3f614d64ca7bd3e71ab2454608c75a
Glen Willis <glenwillis@gmail.com>
2017-10-12 21:22:13 +0800

    Word counter works.

--- KaranNumberProjects/WordCounter.cs
+++ KaranNumberProjects/WordCounter.cs
@@ -10,11 +10,43 @@ namespace KaranNumberProjects
     {
         public static void WordCounterImplementation()
         {
-            string inputString = "Test this is a test of my thing";
+            string inputString = "Test this is a test of my thing.";
 
-            string[] inputStringArray = inputString.Split(' ');
-            var count = inputStringArray.Select(x => x.Count());
-            Console.Write(count);
+            //Convert inputstring to array split up by spaces - removes punctuation
+            string[] inputStringArray = inputString.ToLower().Replace('.',' ').Replace(',', ' ').Replace('!', ' ').Replace('?', ' ').Split(' ');
+
+            //Gets the distinct values of the input string array
+            var inputStringArrayWithDistinctElements = inputStringArray.Distinct().ToArray();
+
+            //Creates a dictionary to hold the word counter
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+
+            //Initialises the dictionary with unique words at count 0
+            foreach (var item in inputStringArrayWithDistinctElements)
+            {
+                if(item != "")
+                {
+                    wordCount.Add(item, 0);
+                }
+            }
+
+            //Checks against the inputstring array with the unique dictionary and increments the value everytime the word is used
+            foreach (var item in inputStringArray)
+            {
+                if (item != "")
+                {
+                    wordCount[item]++;
+                }
+            }
+
+
+            //Iterates through the dictionary to print out the count for each word
+            foreach (var item in wordCount)
+            {
+                Console.WriteLine("{0} is in the string {1} times", item.Key, item.Value);
+            }
+
 
             Console.ReadKey();
         }
